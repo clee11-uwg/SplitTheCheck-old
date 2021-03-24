37251088dd8c0d00aad59b1071be45d7ed473f75
clee11 <clee11@cs6242-vm07.uwg.westga.edu>
2021-03-24 11:54:04 -0400

    added seed test data into the seeds file in order to consistently have the same test data when working with the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+
+Restaurant.create!(
+  name: 'Burger Joint',
+  address: '123 Main Road',
+  city: 'Macon',
+  state: 'GA',
+  zip: '32154',
+  upVoteCount: 40,
+  downVoteCount: 25
+)
+Restaurant.create!(
+  name: 'The Bar',
+  address: '245 Main Road',
+  city: 'Macon',
+  state: 'GA',
+  zip: '32154',
+  upVoteCount: 35,
+  downVoteCount: 30
+)
+Restaurant.create!(
+  name: 'The Rooftop',
+  address: '987 Second Street',
+  city: 'Macon',
+  state: 'GA',
+  zip: '32154',
+  upVoteCount: 30,
+  downVoteCount: 35
+)
+Restaurant.create!(
+  name: 'The Sushi Place',
+  address: '654 Third Street',
+  city: 'Macon',
+  state: 'GA',
+  zip: '32154',
+  upVoteCount: 25,
+  downVoteCount: 40
+)
