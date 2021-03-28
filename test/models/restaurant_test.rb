b3b250cbc7b56f572cf0aa31877bb1187b04e65c
clee11 <clee11@cs6242-vm02.uwg.westga.edu>
2021-03-28 11:41:56 -0400

    added new model test to make sure that the validation for the 5 fields is correct

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "restaurant attributes must not be empty" do
+    restaurant = Restaurant.new
+    assert restaurant.invalid?
+    assert restaurant.errors[:name].any?
+    assert restaurant.errors[:address].any?
+    assert restaurant.errors[:city].any?
+    assert restaurant.errors[:state].any?
+    assert restaurant.errors[:zip].any?
+  end
 end
