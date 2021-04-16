25ee5f741d5ba3a5bfbb7bc389e5fc9f89fd1bbe
clee11 <clee11@cs6242-vm02.uwg.westga.edu>
2021-04-16 12:15:06 -0400

    working on getting the search feature to work

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, :city, :state, :zip, presence: true
+
+  def self.search(search)
+    if (search)
+
+    else
+
+    end
+  end
 end
