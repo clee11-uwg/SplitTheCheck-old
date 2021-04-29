a6337dd75479da84afed6ca903200704e4f299b4
clee11 <clee11@cs6242-vm08.uwg.westga.edu>
2021-04-29 14:41:06 -0400

    modified restaurant show page after discussion with professor for clarification. just need to figure out how to do it for each user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -80,11 +80,13 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
+
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
